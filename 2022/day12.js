ccaba98fa6c38dd4d6e01ea88b1dcdfb3639eacb
Amel <avdic.amel@gmail.com>
2022-12-12 10:08:09 +0100

    Update day12.js

--- 2022/day12.js
+++ 2022/day12.js
@@ -2,3 +2,60 @@
 //https://adventofcode.com/2022/day/12/input
 
 const input = document.querySelector("body > pre").innerHTML.trim();
+
+const dirs = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+];
+
+function solve(input, part) {
+  const starts = [];
+  let end;
+  const map = input.split('\n').map((line, i) =>
+    line.split('').map((char, j) => {
+      let elevation;
+      if (char === 'S' || (part === 2 && char === 'a')) {
+        elevation = 0;
+        starts.push([i, j]);
+      } else if (char === 'E') {
+        elevation = 25;
+        end = [i, j];
+      } else {
+        elevation = char.codePointAt(0) - 'a'.codePointAt(0);
+      }
+      return elevation;
+    })
+  );
+
+  const queue = starts.map((start) => ({ pos: start, steps: 0 }));
+  const seen = [];
+  while (queue.length) {
+    const {
+      pos: [i, j],
+      steps,
+    } = queue.shift();
+    if (seen[i]?.[j]) {
+      continue;
+    }
+    if (i === end[0] && j === end[1]) {
+      console.log(steps);
+      break;
+    }
+    for (const [di, dj] of dirs) {
+      if (
+        map[i + di]?.[j + dj] === undefined ||
+        map[i + di][j + dj] > map[i][j] + 1 ||
+        seen[i + di]?.[j + dj]
+      ) {
+        continue;
+      }
+      queue.push({ pos: [i + di, j + dj], steps: steps + 1 });
+    }
+    seen[i] = seen[i] ?? [];
+    seen[i][j] = 1;
+  }
+}
+solve(input, 1);
+solve(input, 2);
